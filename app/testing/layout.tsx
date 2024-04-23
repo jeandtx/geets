91cee51c42f07195d7c6705eee27c5a04b0ff3e1
jeandtx <jean.doutriaux@gmail.com>
2024-04-23 14:53:25 +0200

    ,

--- app/testing/layout.tsx
+++ app/testing/layout.tsx
@@ -1,16 +1,3 @@
-const links = [
-	{ name: "Open roles", href: "#" },
-	{ name: "Internship program", href: "#" },
-	{ name: "Our values", href: "#" },
-	{ name: "Meet our leadership", href: "#" },
-];
-const stats = [
-	{ name: "Offices worldwide", value: "12" },
-	{ name: "Full-time colleagues", value: "300+" },
-	{ name: "Hours per week", value: "40" },
-	{ name: "Paid time off", value: "Unlimited" },
-];
-
 const TestingLayout = ({ children }: { children: React.ReactNode }) => {
 	return (
 		<div className="bg-gray-500 flex flex-col items-center justify-center min-h-screen">
