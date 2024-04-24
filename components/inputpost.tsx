7ff58c0076295fa48808003bfdece8293ae0bec8
cleophass <cleophas.fournier@efrei.net>
2024-04-24 22:39:52 +0200

    styling

--- components/inputpost.tsx
+++ components/inputpost.tsx
@@ -1,6 +1,8 @@
 "use client";
 import React, { useState } from 'react';
 import { Input } from "./ui/input";
+import { TextArea } from "./ui/textarea";
+import { Button } from "./ui/button";
 
 export function InputPost() {
     const [fullname, setFullname] = useState('')
@@ -10,6 +12,7 @@ export function InputPost() {
 
     const handleSubmit = async (e: React.FormEvent) => {
         e.preventDefault()
+        
         console.log(fullname, email, message)
 
         const res = await fetch("api/posts", {
@@ -36,26 +39,36 @@ export function InputPost() {
         <form onSubmit={handleSubmit} className='flex flex-col gap-5'>
             <div>
                 <label htmlFor="fullname">Full Name</label>
-                <input onChange={(e) => setFullname(e.target.value)}
+                <Input onChange={(e) => setFullname(e.target.value)}
                 value={fullname}
-                type='text' id='fullname' placeholder='john' /> 
+                type='text' id='fullname' placeholder='your name' />
+            
             </div>
 
             <div>
                 <label htmlFor="email">Email</label>
-                <input onChange={(e) => setEmail(e.target.value)}
+                <Input onChange={(e) => setEmail(e.target.value)}
                 value={email}
-                type='email' id='email' placeholder='john@mail.com'/>
+                type='email' id='email' placeholder='your mail'
+                />
+
+
+
+                
             </div>
 
             <div>
                 <label htmlFor="message">Message</label>
-                <textarea onChange={(e) => setMessage(e.target.value)}
+                <TextArea onChange={(e) => setMessage(e.target.value)}
                 value={message}
-                id='message' placeholder='your message here'></textarea>
+                id='message' placeholder='your message here' />
+                
+
+
+                
             </div>
 
-            <button type='submit'>Send</button>
+            <Button type='submit'>Send</Button>
 
             <div>Error</div>
             
