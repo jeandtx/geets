2785b6d5ba7849118c014dccd67f11c2d1b2f27f
cleophass <cleophas.fournier@efrei.net>
2024-04-26 20:33:31 +0200

    .

---
+++ app/api/posts/route.ts
@@ -0,0 +1,25 @@
+import { NextResponse } from "next/server";
+import clientPromise from "../../../lib/mongodb";
+
+export async function POST(req: Request) {
+    const { title, hook, theme, description } = await req.json();
+    const client = await clientPromise;
+    const db = client.db('geets'); 
+    try {
+        if (!title || title.length < 3 || title.length > 100) {
+            throw new Error("Title must be between 3 and 100 characters.");
+        }
+        if (!hook || hook.trim() === "") {
+            throw new Error("Hook is required.");
+        }
+        if (!theme || theme.trim() === "") {
+            throw new Error("Theme is required.");
+        }
+
+        const result = await db.collection('posts').insertOne({ title, hook, theme, description, date: new Date() });
+        return NextResponse.json({ msg: ['Post created successfully'], success: true });
+    } catch (error: any) {
+        console.error(error);
+        return NextResponse.json({ msg: [error.message], success: false });
+    }
+};
--- components/inputpost.tsx
+++ components/inputpost.tsx
@@ -1,20 +1,21 @@
 "use client";
 import React, { useState, useEffect, useRef } from 'react';
-import { Input } from "./ui/input";
-import { TextArea } from "./ui/textarea";
 import { Button } from "./ui/button";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar"
+import { Input } from "./ui/input"
 
 export function InputPost() {
-    const [fullname, setFullname] = useState('');
-    const [email, setEmail] = useState('');
-    const [message, setMessage] = useState('');
+    const [title, setTitle] = useState('');
+    const [hook, setHook] = useState('');
+    const [theme, setTheme] = useState('');
+    const [description, setDescription] = useState('');
     const [isExpanded, setIsExpanded] = useState(false);
 
     const formRef = useRef<HTMLFormElement>(null);
 
     useEffect(() => {
         const handleClickOutside = (event: MouseEvent) => {
-            if (formRef.current && !formRef.current.contains(event.target as Node)) {  // Type assertion for event.target
+            if (formRef.current && !formRef.current.contains(event.target as Node)) {
                 setIsExpanded(false);
             }
         };
@@ -28,27 +29,43 @@ export function InputPost() {
         };
     }, [isExpanded]);
 
-    const handleFullnameFocus = () => {
+    const handleTitleFocus = () => {
         setIsExpanded(true);
     };
 
     const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
-        
-        console.log(fullname, email, message);
-
+    
+        // Vérification des conditions avant de procéder
+        const isTitleValid = title.length > 3;
+        const isHookValid = hook.trim() !== '';
+        const isThemeValid = theme.trim() !== '';
+    
+        // Log l'état des champs
+        console.log(`Title: ${title}, Valid: ${isTitleValid}`);
+        console.log(`Hook: ${hook}, Valid: ${isHookValid}`);
+        console.log(`Theme: ${theme}, Valid: ${isThemeValid}`);
+    
+        // Vérifie si tous les champs sont valides
+        if (!isTitleValid || !isHookValid || !isThemeValid) {
+            console.log("Validation failed, not submitting.");
+            return;  // Ne pas soumettre les données si la validation échoue
+        }
+    
+        // Si tout est valide, envoyer la requête
         const res = await fetch("api/posts", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
             },
             body: JSON.stringify({
-                fullname,
-                email,
-                message
+                title,
+                hook,
+                theme,
+                description
             }),
         });
-
+    
         const data = await res.json();
         if (data.error) {
             console.log(data.msg);
@@ -56,51 +73,83 @@ export function InputPost() {
             console.log('Submission successful');
         }
     };
-
+    const expandedStyle = {
+        maxHeight: isExpanded ? '1000px' : '0', // Adjust max height based on your content size
+        overflow: 'hidden',
+        transition: 'max-height 0.5s ease-in-out'
+    };
+    
+    
     return (
         <>
-        <div className="border-2 p-5 rounded-3xl border-black">
-        <form ref={formRef} onSubmit={handleSubmit} className='flex flex-col gap-5'>
-            <div>
-                <label htmlFor="fullname">Full Name</label>
-                <Input 
-                    onFocus={handleFullnameFocus}
-                    onChange={(e) => setFullname(e.target.value)}
-                    value={fullname}
+        <div className="p-5 rounded-3xl bg-postbg">
+        <form ref={formRef} onSubmit={handleSubmit} className='flex flex-col'>
+            <div className="flex items-center gap-2">
+                <Avatar>
+                    <AvatarImage src="https://github.com/shadcn.png" />
+                    <AvatarFallback>CN</AvatarFallback>
+                </Avatar>
+                <input 
+                    onFocus={handleTitleFocus}
+                    onChange={(e) => setTitle(e.target.value)}
+                    value={title}
                     type='text' 
-                    id='fullname' 
-                    placeholder='your name' 
+                    id='title' 
+                    placeholder='Share something !' 
+                    className='text-black text-inter placeholder-gray-400 font-normal rounded-full flex h-10 w-full border border-input bg-background px-3 py-2 text-sm focus:outline-none focus:ring-0 focus:shadow-none focus:border-gray-300'
                 />
             </div>
 
-            {isExpanded && (
-                <>
+            <div style={expandedStyle} className='flex flex-col gap-5'>
+                
                     <div>
-                        <label htmlFor="email">Email</label>
-                        <Input 
-                            onChange={(e) => setEmail(e.target.value)}
-                            value={email}
-                            type='email' 
-                            id='email' 
-                            placeholder='your mail'
+                        <textarea 
+                            onChange={(e) => setHook(e.target.value)}
+                            value={hook}
+                            id='hook' 
+                            placeholder='Enter a catchy hook'
+                            style={{ height: '60px', borderRadius : '15px'}}
+                            className='mt-4 flex w-full placeholder-gray-400 font-normal rounded-md border border-input bg-background px-3 py-2 text-sm focus:outline-none focus:border-gray-300 focus:ring-0 disabled:cursor-not-allowed disabled:opacity-50'
                         />
                     </div>
 
                     <div>
-                        <label htmlFor="message">Message</label>
-                        <TextArea 
-                            onChange={(e) => setMessage(e.target.value)}
-                            value={message}
-                            id='message' 
-                            placeholder='your message here' 
+                        <input 
+                            onChange={(e) => setTheme(e.target.value)}
+                            value={theme}
+                            type='text' 
+                            id='theme' 
+                            placeholder='Post theme'
+                            className='text-black text-inter placeholder-gray-400 font-normal rounded-full flex h-10 w-full border border-input bg-background px-3 py-2 text-sm focus:outline-none focus:ring-0 focus:shadow-none focus:border-gray-300'
                         />
                     </div>
 
-                    <Button type='submit'>Send</Button>
-                </>
-            )}
+                    <div>
+                        <Input
+                            type='file'
+                            id='file'
+                            placeholder='Upload a file'
+                            className='text-black text-inter placeholder-gray-400 font-normal rounded-full flex h-10 w-full border border-input bg-background px-3 py-2 text-sm focus:outline-none focus:ring-0 focus:shadow-none focus:border-gray-300'
+                        />
+                    </div>
+
+                    <div>
+                        <textarea 
+                            onChange={(e) => setDescription(e.target.value)}
+                            value={description}
+                            id='description' 
+                            placeholder='Detailed description (optional)' 
+                            style={{ height: '120px' ,borderRadius : '15px' }}
+                            className='flex w-full rounded-md placeholder-gray-400 font-normal border border-input bg-background px-3 py-2 text-sm focus:outline-none focus:border-gray-300 focus:ring-0 disabled:cursor-not-allowed disabled:opacity-50'
+                        />
+                    </div>
+
+                    <Button type='submit'>Submit</Button>
+                
+            </div>
         </form>
         </div>
+        
         </>
     );
 }
