d7028dce8f6c9a6984427e0bc1bc5b7ffa04c192
jeandtx <jean.doutriaux@gmail.com>
2024-04-30 11:53:27 +0200

    putting fetch into posts components

--- app/movies/page.tsx
+++ app/movies/page.tsx
@@ -1,85 +1,52 @@
-import { Metadata } from 'next'
-
+import { Metadata } from "next";
 import clientPromise from "../../lib/mongodb";
-
-import { SideProjectCard } from "@/components/project_card";
+import SideProjectCard from "@/components/project_card";
 
 export const metadata: Metadata = {
-    title: 'Top 15 Movies by Metacritic',
-}
+	title: "Top 15 Movies by Metacritic",
+};
 
 interface User {
-    _id: string;
-    age: string;
-    email: string;
-    Media: string;
-    pseudo: string;
-    Rating: number;
-    UserId : number;
-    location: string;
+	_id: string;
+	age: string;
+	email: string;
+	Media: string;
+	pseudo: string;
+	Rating: number;
+	UserId: number;
+	location: string;
 }
-
-
 
 async function getPosts() {
-    const client = await clientPromise;
-    const db = client.db("geets");
-    const posts = await db
-        .collection("posts_fake")
-        .find({})
-        .sort({ metacritic: -1 })
-        // .limit(85)
-        .toArray();
-    return posts;
+	const client = await clientPromise;
+	const db = client.db("geets");
+	const posts = await db
+		.collection("posts_fake")
+		.find({})
+		.sort({ metacritic: -1 })
+		.limit(10)
+		.toArray();
+	return posts;
 }
-
-async function getUserById(userId: string): Promise<User> {
-    const client = await clientPromise;
-    const db = client.db("geets");
-    const user = await db
-        .collection<User>("users")
-        .findOne({ _id: userId });
-    return user!;
-}
-
-// const user = {
-//     _id: { $oid: "662a25dfe36d3727d331f9d0" },
-// Name: "Jean Dtx",
-// Email: "jean.dtsnitch@pipi.caca",
-// media: "https://images.unsplash.com/photo-1558723223-0f8c63ea0fed?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w1OTQ2NjB8MHwxfHNlYXJjaHwxfHxNYWdpYyUyMCUyNiUyMElsbHVzaW9ufGVufDB8fHx8MTcxNDAzODIwOHww&ixlib=rb-4.0.3&q=80&w=1080",
-// Posts: "4",
-// Rating: 5
-// }
-
-
-
 
 export default async function Dashboard() {
-    const posts = await getPosts()
-    const user = await getUserById(posts[2].userId)
-    // make a list of the same length of posts with the user of each post thanks to the function getUserById
-    const users = posts.map((post) => getUserById(post.userId))
-    // for loop to get the user of each post
-    // for (let i = 0; i < posts.length; i++) {
+	const posts = await getPosts();
+	const postList = posts.map((post) => (
+		<>
+			<div className="flex p-8 bg-gray-50  overflow-hidden">
+				<SideProjectCard post={post} />
+			</div>
+		</>
+	));
 
-    console.log(user)
-    "use client"
-    const postList = posts.map((post) => (
-        // for each post create new user const user = getUserById(post.userId)
-        
-        
-        <>
-        
-            <div className="flex p-8 bg-gray-50  overflow-hidden">
-			    <SideProjectCard post={post} user={user}/>
-		    </div>
-        </>
-    ));
-
-    return (
-        <div className="py-8">
-            <h1 className="title text-green-400 text-center text-3xl font-bold mb-16">Top 15 posts by Metacritic</h1>
-            <ul className="flex flex-wrap justify-center m-0 p-0 list-none">{postList}</ul>
-        </div>
-    );
+	return (
+		<div className="py-8">
+			<h1 className="title text-green-400 text-center text-3xl font-bold mb-16">
+				Top 15 posts by Metacritic
+			</h1>
+			<ul className="flex flex-wrap justify-center m-0 p-0 list-none">
+				{postList}
+			</ul>
+		</div>
+	);
 }
--- components/project_card.tsx
+++ components/project_card.tsx
@@ -1,79 +1,122 @@
-import React from 'react';
-import 'bulma/css/bulma.min.css';
-import { list } from 'postcss';
-
+import React from "react";
+// import 'bulma/css/bulma.min.css';
+import { list } from "postcss";
+import clientPromise from "@/lib/mongodb";
+import Img from "next/image";
 
 interface SideProjectPost {
-  _id: {
-    $oid: string;
-  };
-  time: {
-    $date: string;
-  };
-  title: string;
-  hook: string;
-  theme: string;
-  description: string;
-  media: string;
-  labels: string[];
-  participants: number;
+	_id: {
+		$oid: string;
+	};
+	time: {
+		$date: string;
+	};
+	title: string;
+	hook: string;
+	theme: string;
+	description: string;
+	media: string;
+	labels: string[];
+	participants: number;
 }
 
 interface SideProjectUser {
-  _id: {
-    $oid: string;
-  };
-  name: string;
-  email: string;
-  experience: number;
-  media: string;
-  PostId: string[];
-  Media: string;
-  
+	_id: {
+		$oid: string;
+	};
+	name: string;
+	email: string;
+	experience: number;
+	media: string;
+	PostId: string[];
+	Media: string;
+}
 
+interface User {
+	_id: string;
+	age: string;
+	email: string;
+	Media: string;
+	pseudo: string;
+	Rating: number;
+	UserId: number;
+	location: string;
 }
 
 interface SideProjectCardProps {
-  post: any;
-  user: any;
+	post: any;
+	// user: any;
 }
 
-export const SideProjectCard: React.FC<SideProjectCardProps> = ({ post, user}) => {
-  return (
-    // Parent container with horizontal layout, rounded corners, shadow, and a defined maximum width
-    <div className="flex max-w-2xl rounded overflow-hidden shadow-lg">
+async function getUserById(userId: string): Promise<User> {
+	const client = await clientPromise;
+	const db = client.db("geets");
+	const user = await db.collection<User>("users").findOne({ _id: userId });
+	return user!;
+}
 
-      {/* Image container with a relative size */}
-      <div className="w-2/5">
-        <img className="w-full h-full object-cover" src={post.media ? post.media : "https://bulma.io/images/placeholders/1280x960.png"} alt="Placeholder image" />
-      </div>
+export default async function SideProjectCard({
+	post, // user,
+}: SideProjectCardProps) {
+	const user = await getUserById(post.userId);
+	return (
+		// Parent container with horizontal layout, rounded corners, shadow, and a defined maximum width
+		<div className="flex max-w-2xl rounded overflow-hidden shadow-lg">
+			{/* Image container with a relative size */}
+			<div className="w-2/5">
+				<Img
+					className="w-full h-full object-cover"
+					src={
+						post.media
+							? post.media
+							: "https://bulma.io/images/placeholders/1280x960.png"
+					}
+					alt="Placeholder image"
+				/>
+			</div>
 
-      {/* Text content container with padding and background color */}
-      <div className="w-3/5 p-2 bg-gray-100">
-        {/* put a rounded image placer for user.media */}
-        <div className="flex items-center mb-4">
-          <img className="w-12 h-12 rounded-full mr-4" src={user.Media ? user.Media : "https://bulma.io/images/placeholders/96x96.png"} alt="Placeholder image" />
-          <div>
-            {/* put user.Name and next of it the user.Rating over 5*/}
-            <p className="text-lg text-gray-900 font-bold">{user.name} {user.Rating}/5</p>
+			{/* Text content container with padding and background color */}
+			<div className="w-3/5 p-2 bg-gray-100">
+				{/* put a rounded image placer for user.media */}
+				<div className="flex items-center mb-4">
+					<Img
+						className="w-12 h-12 rounded-full mr-4"
+						src={
+							user.Media
+								? user.Media
+								: "https://bulma.io/images/placeholders/96x96.png"
+						}
+						alt="Placeholder image"
+					/>
+					<div>
+						{/* put user.Name and next of it the user.Rating over 5*/}
+						<p className="text-lg text-gray-900 font-bold">
+							{user.email} 4/5
+						</p>
 
+						{/* <p className="text-lg text-gray-900 font-bold">{user.Name}</p>  */}
+						<p className="text-sm text-gray-600">{user.email}</p>
+						{/* write the number of posts  */}
+						<p className="text-sm text-gray-600">
+							{user.age} posted projects
+						</p>
+					</div>
+				</div>
+				{/*make a new div for the content with padding*/}
 
-            {/* <p className="text-lg text-gray-900 font-bold">{user.Name}</p>  */}
-            <p className="text-sm text-gray-600">{user.email}</p>
-            {/* write the number of posts  */}
-            <p className="text-sm text-gray-600">{user.PostId} posted projects</p>
-          </div>
-        </div>
-        {/*make a new div for the content with padding*/}
-
-        <div className="p-2">
-        <h2 className="text-2xl font-bold mb-2">{post.title}</h2>
-        <p className="text-base text-gray-700 underline mb-1">{post.hook}</p>
-        <p className="text-gray-700 mb-0">{post.description}</p>
-        <p className="text-sm text-gray-600 mt-4"><time dateTime={post.time.$date}>{new Date(post.time.$date).toLocaleDateString()}</time></p>
-        </div>
-      </div>
-
-    </div>
-  );
+				<div className="p-2">
+					<h2 className="text-2xl font-bold mb-2">{post.title}</h2>
+					<p className="text-base text-gray-700 underline mb-1">
+						{post.hook}
+					</p>
+					<p className="text-gray-700 mb-0">{post.description}</p>
+					<p className="text-sm text-gray-600 mt-4">
+						<time dateTime={post.time.$date}>
+							{new Date(post.time.$date).toLocaleDateString()}
+						</time>
+					</p>
+				</div>
+			</div>
+		</div>
+	);
 }
