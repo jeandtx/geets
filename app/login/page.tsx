65c41fb3587f4dcd987f72807135c91f07133bf3
jeandtx <jean.doutriaux@gmail.com>
2024-04-30 17:14:00 +0200

    handle bad auth case

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,37 +1,56 @@
-import Link from 'next/link';
-import { Form } from '@/components/form';
-import { signIn } from '@/app/auth';
-import { SubmitButton } from '@/components/ui/submit-button';
+import Link from "next/link";
+import { Form } from "@/components/form";
+import { signIn } from "@/app/auth";
+import { SubmitButton } from "@/components/ui/submit-button";
+import { useToast } from "@/components/ui/use-toast";
 
 export default function Login() {
-  return (
-    <div className="flex h-screen w-screen items-center justify-center mt-5">
-      <div className="z-10 w-full max-w-md overflow-hidden rounded-2xl border border-gray-100 shadow-xl">
-      <div className="flex flex-col space-y-1 bg-white px-4 py-6 pt-8 sm:px-16 ">
-        <h3 className="text-xl font-semibold">Sign In</h3>
-      </div>
-        <Form
-          action={async (formData: FormData) => {
-            'use server';
-            await signIn('credentials', {
-              redirectTo: '/protected',
-              email: formData.get('email') as string,
-              password: formData.get('password') as string,
-            });
-          }}
-        >
-          <SubmitButton style={{ backgroundColor: 'rgb(58, 93, 240)', color: 'white' }}>Sign In</SubmitButton>
-          <div className="flex flex-row justify-center space-x-4">
-            <p className="text-center text-sm text-plaintext">Forget Password? </p>
-            <p className="text-center text-sm text-plaintext">
-              {"Don't have an account? "}
-              <Link href="/register" className="text-textblue">
-                Sign up
-              </Link>
-            </p>
-          </div>
-        </Form>
-      </div>
-    </div>
-  );
+	const { toast } = useToast();
+	return (
+		<div className="flex h-screen w-screen items-center justify-center mt-5">
+			<div className="z-10 w-full max-w-md overflow-hidden rounded-2xl border border-gray-100 shadow-xl">
+				<div className="flex flex-col space-y-1 bg-white px-4 py-6 pt-8 sm:px-16 ">
+					<h3 className="text-xl font-semibold">Sign In</h3>
+				</div>
+				<Form
+					action={async (formData: FormData) => {
+						"use server";
+						try {
+							await signIn("credentials", {
+								redirectTo: "/protected",
+								email: formData.get("email") as string,
+								password: formData.get("password") as string,
+							});
+						} catch (error) {
+							toast({
+								variant: "destructive",
+								title: "Uh oh! Something went wrong.",
+								description: "error",
+							});
+						}
+					}}
+				>
+					<SubmitButton
+						style={{
+							backgroundColor: "rgb(58, 93, 240)",
+							color: "white",
+						}}
+					>
+						Sign In
+					</SubmitButton>
+					<div className="flex flex-row justify-center space-x-4">
+						<p className="text-center text-sm text-plaintext">
+							Forget Password?{" "}
+						</p>
+						<p className="text-center text-sm text-plaintext">
+							{"Don't have an account? "}
+							<Link href="/register" className="text-textblue">
+								Sign up
+							</Link>
+						</p>
+					</div>
+				</Form>
+			</div>
+		</div>
+	);
 }
