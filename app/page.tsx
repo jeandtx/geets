d461ac9ce3696a42bb6aaca859d9a3614ea195d2
soanja <soanja.mirindra.johanna.ravalisaona@efrei.net>
2024-05-03 15:17:22 +0200

    adding the component upper tab

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,6 @@ import { InfiniteScroll } from "@/components/infinitescroll";
 export default async function Home() {
 	return (
 		<main className="flex flex-col w-full h-screen bg-gray-200">
-			<div className="w-full h-12 bg-blue-200 flex justify-center items-center">
-				top bar
-			</div>
 			<div className="flex w-full h-full bg-gray-200 overflow-hidden">
 				<Sidebar className="w-1/5 bg-gray-200" />
 				<div className="flex justify-evenly items-center w-4/5">
--- app/testing/page.tsx
+++ app/testing/page.tsx
@@ -1,5 +1,7 @@
 import { Metadata } from "next";
 import { InputPost } from "@/components/inputpost";
+import { UpperBar } from "@/components/upper-bar";
+import { Sidebar } from "@/components/sidebar";
 
 export const metadata: Metadata = {
 	title: "Test zone",
@@ -7,8 +9,13 @@ export const metadata: Metadata = {
 
 export default async function Dashboard() {
 	return (
-		<div className="p-5 ">
+		<div className="p-5">
 			<InputPost />
+			<div className="inline-flex">
+				<Sidebar className="w-1/5 bg-gray-200" />
+				<UpperBar className="w-3/5 h-20" />
+				<div className="bg-red-500 w-1/5">aaa</div>
+			</div>
 		</div>
 	);
 }
