1f1e2300c56fbf10e4673d20faed0767bc37100e
jeandtx <jean.doutriaux@gmail.com>
2024-05-05 17:28:52 +0200

    extruded data from card component

--- components/card.tsx
+++ components/card.tsx
@@ -15,22 +15,26 @@ import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import BadgeList from "@/components/ui/badge";
 import Img from "next/image";
 
-const jsonData = {
-	Title: "Tricks of the Trade",
-	Time: {
-		$date: "2023-06-28T09:09:58Z",
-	},
-	Thème: "Magic & Illusion",
-	Date: "Learn to perform magic tricks and illusions that will amaze your friends and family, from beginner to advanced levels.",
-	Media: "https://images.unsplash.com/photo-1558723223-0f8c63ea0fed?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w1OTQ2NjB8MHwxfHNlYXJjaHwxfHxNYWdpYyUyMCUyNiUyMElsbHVzaW9ufGVufDB8fHx8MTcxNDAzODIwOHww&ixlib=rb-4.0.3&q=80&w=1080",
-	Labels: ["event", "when", "agreement"],
-	Participants: 2,
-};
+interface CardDemoProps extends React.HTMLAttributes<HTMLDivElement> {
+	className?: string;
+	jsonData: {
+		Title: string;
+		Time: {
+			$date: string;
+		};
+		Thème: string;
+		Date: string;
+		Media: string;
+		Labels: string[];
+		Participants: number;
+	};
+}
 
 export function CardDemo({
 	className,
+	jsonData,
 	...props
-}: Readonly<React.HTMLAttributes<HTMLDivElement>>) {
+}: Readonly<CardDemoProps>) {
 	return (
 		<div className="flex flex-col">
 			<Card
@@ -69,13 +73,16 @@ export function CardDemo({
 						/>
 					</CardImage>
 
-					<ul className="mt-2 text-sm text-gray-600 flex justify-between">
+					<div className="mt-2 text-sm text-gray-600 flex justify-between">
 						<BadgeList
 							jsonData={{ Labels: jsonData.Labels }}
 						></BadgeList>
-					</ul>
+					</div>
 					<div className="mt-2">
-						<strong>Participants:</strong> {jsonData.Participants}
+						<div className="text-sm text-gray-600 flex justify-between">
+							Participants:
+						</div>{" "}
+						{jsonData.Participants}
 					</div>
 				</CardContent>
 
