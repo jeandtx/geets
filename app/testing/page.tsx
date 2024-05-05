c314f84a50c5fbeca44807ade2cad8617e99d9bd
jeandtx <jean.doutriaux@gmail.com>
2024-05-05 17:21:56 +0200

    chore: Remove unused components and update image remote patterns

--- app/testing/page.tsx
+++ app/testing/page.tsx
@@ -1,8 +1,5 @@
 import { Metadata } from "next";
-import { RandomComponent } from "@/components/random";
-import { Sidebar } from "@/components/sidebar";
 import { CardDemo } from "@/components/card";
-import { InputPost } from "@/components/inputpost";
 
 export const metadata: Metadata = {
 	title: "Test zone",
@@ -11,8 +8,6 @@ export const metadata: Metadata = {
 export default async function Dashboard() {
 	return (
 		<div className="flex p-8 bg-gray-50">
-			{/* <Sidebar /> */}
-			{/* <InputPost /> */}
 			<CardDemo />
 		</div>
 	);
--- components/card.tsx
+++ components/card.tsx
@@ -1,73 +1,90 @@
-"use client"
-import React from 'react';
-import { Card, CardContent, CardDate, CardFooter, CardHeader, CardTitle, CardImage } from "@/components/ui/card";
+"use client";
+import React from "react";
+import {
+	Card,
+	CardContent,
+	CardDate,
+	CardFooter,
+	CardHeader,
+	CardTitle,
+	CardImage,
+} from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { Send } from "lucide-react";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import BadgeList from '@/components/ui/badge';
+import BadgeList from "@/components/ui/badge";
+import Img from "next/image";
 
 const jsonData = {
-  Title: "Tricks of the Trade",
-  Time: {
-    $date: "2023-06-28T09:09:58Z",
-  },
-  Thème: "Magic & Illusion",
-  Date:
-    "Learn to perform magic tricks and illusions that will amaze your friends and family, from beginner to advanced levels.",
-  Media:
-    "https://images.unsplash.com/photo-1558723223-0f8c63ea0fed?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w1OTQ2NjB8MHwxfHNlYXJjaHwxfHxNYWdpYyUyMCUyNiUyMElsbHVzaW9ufGVufDB8fHx8MTcxNDAzODIwOHww&ixlib=rb-4.0.3&q=80&w=1080",
-  Labels: ["event", "when", "agreement"],
-  Participants: 2,
+	Title: "Tricks of the Trade",
+	Time: {
+		$date: "2023-06-28T09:09:58Z",
+	},
+	Thème: "Magic & Illusion",
+	Date: "Learn to perform magic tricks and illusions that will amaze your friends and family, from beginner to advanced levels.",
+	Media: "https://images.unsplash.com/photo-1558723223-0f8c63ea0fed?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w1OTQ2NjB8MHwxfHNlYXJjaHwxfHxNYWdpYyUyMCUyNiUyMElsbHVzaW9ufGVufDB8fHx8MTcxNDAzODIwOHww&ixlib=rb-4.0.3&q=80&w=1080",
+	Labels: ["event", "when", "agreement"],
+	Participants: 2,
 };
 
 export function CardDemo({
-  className,
-  ...props
-}: React.HTMLAttributes<HTMLDivElement>) {
-  return (
+	className,
+	...props
+}: Readonly<React.HTMLAttributes<HTMLDivElement>>) {
+	return (
+		<div className="flex flex-col">
+			<Card
+				className={`border border-gray-200 rounded-lg p-4 shadow-sm ${className}`}
+				{...props}
+			>
+				<CardHeader className="flex justify-between">
+					<Avatar>
+						<AvatarImage
+							src="https://github.com/shadcn.png"
+							alt="@shadcn"
+						/>
+						<AvatarFallback>CN</AvatarFallback>
+					</Avatar>
+					<CardTitle className="text-lg font-bold">
+						{jsonData.Title}
+					</CardTitle>
 
-    <div className="flex flex-col">
-      <Card className={`border border-gray-200 rounded-lg p-4 shadow-sm ${className}`}{...props}>
-      
-      <CardHeader className="flex justify-between">
-        
-      <Avatar>
-        <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
-        <AvatarFallback>CN</AvatarFallback>
-      </Avatar>
-        <CardTitle className="text-lg font-bold">{jsonData.Title}</CardTitle>
-        
-          <CardDate className="text-sm text-gray-500">
-              {new Date(jsonData.Time.$date).toLocaleString()}
-            </CardDate>
-        </CardHeader>
+					<CardDate className="text-sm text-gray-500">
+						{new Date(jsonData.Time.$date).toLocaleString()}
+					</CardDate>
+				</CardHeader>
 
-        <CardContent className="mt-4">
-        <p className="text-gray-700 font-medium">{jsonData.Date}</p>
-        <CardImage><img
-            src={jsonData.Media}
-            alt={jsonData.Title}
-            className="flex justify-center items-center rounded-md mt-4 "
-            style={{ width: "30%", height: "auto" }}
-          /></CardImage> 
-          
-          <ul className="mt-2 text-sm text-gray-600 flex justify-between">
-          <BadgeList jsonData={{ Labels: jsonData.Labels }}></BadgeList>
-          </ul>
-        <p className="mt-2">
-            <strong>Participants:</strong> {jsonData.Participants}
-        </p>
-       </CardContent>
+				<CardContent className="mt-4">
+					<div className="text-gray-700 font-medium">
+						{jsonData.Date}
+					</div>
+					<CardImage>
+						<Img
+							src={jsonData.Media}
+							alt={jsonData.Title}
+							className="flex justify-center items-center rounded-md mt-4 "
+							style={{ width: "30%", height: "auto" }}
+							width={1080}
+							height={720}
+						/>
+					</CardImage>
 
-      <CardFooter className="mt-4">
-        <Button className="w-full">
-          <Send className="mr-2 h-4 w-4" /> Contact the Person
-        </Button>
-      </CardFooter>
-    </Card>
+					<ul className="mt-2 text-sm text-gray-600 flex justify-between">
+						<BadgeList
+							jsonData={{ Labels: jsonData.Labels }}
+						></BadgeList>
+					</ul>
+					<div className="mt-2">
+						<strong>Participants:</strong> {jsonData.Participants}
+					</div>
+				</CardContent>
 
-
-    </div>
-    
-  );
+				<CardFooter className="mt-4">
+					<Button className="w-full">
+						<Send className="mr-2 h-4 w-4" /> Contact the Person
+					</Button>
+				</CardFooter>
+			</Card>
+		</div>
+	);
 }
