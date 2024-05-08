83e837a73a2d3c59e19bec64c789cd32948e4096
jeandtx <jean.doutriaux@gmail.com>
2024-05-08 20:37:24 +0200

    refactor: Remove req from GET routes

--- app/api/[user]/projects/route.ts
+++ app/api/[user]/projects/route.ts
@@ -3,7 +3,7 @@ import clientPromise from "@/lib/mongodb";
 import { Params } from "next/dist/shared/lib/router/utils/route-matcher";
 
 
-export async function GET(req: Request, context: { params: Params }) {
+export async function GET(context: { params: Params }) {
     const { user } = context.params;
     const client = await clientPromise;
     const db = client.db("geets");
