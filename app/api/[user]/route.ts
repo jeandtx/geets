faa84254e43d6a3e5433cfb7d168f6ba36de366e
jeandtx <jean.doutriaux@gmail.com>
2024-05-08 20:45:09 +0200

    refactor: Remove user property from GET route

--- app/api/[user]/route.ts
+++ app/api/[user]/route.ts
@@ -2,10 +2,6 @@ import { NextResponse } from "next/server";
 import clientPromise from "@/lib/mongodb";
 import { Params } from "next/dist/shared/lib/router/utils/route-matcher";
 
-type Request = {
-    user: string;
-};
-
 export async function GET(req: Request, context: { params: Params }) {
     const { user } = context.params;
     const client = await clientPromise;
