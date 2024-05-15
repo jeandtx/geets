9e07b779bbeb7f855f64957d74d99dccb8ed9319
Shadow032020 <asma200318@gmail.com>
2024-05-15 16:08:55 +0200

    edit email

--- app/[profil]/page.tsx
+++ app/[profil]/page.tsx
@@ -3,142 +3,69 @@ import clientPromise from "@/lib/mongodb";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import { getUser } from "@/lib/actions";
+import UpdateProfil from "./updateprofil";
 
 function SignOut() {
-	return (
-		<form
-			action={async () => {
-				"use server";
-				await signOut();
-			}}
-		>
-			<Button variant={"outline"} type="submit">
-				Sign out
-			</Button>
-		</form>
-	);
+  return (
+    <form
+      action={async () => {
+        "use server";
+        await signOut();
+      }}
+    >
+      <Button variant={"outline"} type="submit">
+        Sign out
+      </Button>
+    </form>
+  );
 }
 function EditProfile() {
-	return (
-		<form
-			action={async () => {
-				"use server";
-			}}
-		>
-			<Button variant={"default"} type="submit">
-				Edit my profile
-			</Button>
-		</form>
-	);
+  return (
+    <form
+      action={async () => {
+        "use server";
+      }}
+    >
+      <Button variant={"default"} type="submit">
+        Edit my profile
+      </Button>
+    </form>
+  );
 }
 
-// function EditProfile() {
-// 	function handleEdit() {
-// 		console.log("hellooooooooooooooo");
-// 		throw new Error('Function not implemented.');
-// 	}
-
-// 	return (
-// 		<form
-// 			action={async () => {
-// 				"use server";
-// 				console.log("hiiiiiiiiiiiiiiiiii");
-// 				// useEffect(()=>{
-// 				// 	handleEdit();
-// 				// }, []);
-// 			}
-// 		}
-
-// 		>
-// 			<Button variant={"default"} type="submit">
-// 				Edit my profile
-// 			</Button>
-// 		</form>
-// 	);
-// }
-
-// function EditProfile() {
-// 	// Ajout d'un console.log à la déclaration de la fonction
-// 	console.log('EditProfile component is being rendered');
-
-// 	function handleEdit() {
-// 	  // Ajout d'un console.log dans une fonction
-// 	  console.log('handleEdit function has been called');
-// 	  throw new Error('Function not implemented.');
-// 	}
-
-// 	return (
-// 	  <form
-// 		action={async () => {
-// 		  "use server";
-// 		  console.log('Form action triggered');
-// 		  // Si vous avez un code asynchrone ici, vous pouvez également ajouter des console.log
-// 		  try {
-// 			await handleEdit();
-// 		  } catch (error) {
-// 			console.error('Error in form action:', error);
-// 		  }
-// 		}}
-// 	  >
-// 		{/* Ajout d'un console.log lorsque le bouton est rendu */}
-// 		<Button variant={"default"} type="submit" onClick={() => console.log('Edit my profile button clicked')}>
-// 		  Edit my profile
-// 		</Button>
-// 	  </form>
-// 	);
-//   }
-
 export default async function ProfilPage({
-	params,
+  params,
 }: Readonly<{
-	params: { profil: string };
+  params: { profil: string };
 }>) {
-	const session = await auth();
-	const { profil } = params;
-	const decodeEmail = decodeURIComponent(profil);
-	const user = await getUser(decodeEmail);
+  const { profil } = params;
+  const decodeEmail = decodeURIComponent(profil);
+  const user = await getUser(decodeEmail);
 
-	return (
-		<div className="flex h-screen">
-			<div className="w-screen h-screen flex flex-col space-y-5 justify-center items-center text-black">
-				<div>Search other profils from the database to try</div>
-				<Link href="/sanchezlori@example.com" className="text-textblue">
-					{" "}
-					for example sanchezlori@example.com
-				</Link>
-				<div>
-					Note that you have to be logged in to access this page and
-					any profil page
-				</div>
-				<h1 className="text-3xl font-bold ">Profil of {decodeEmail}</h1>
-				{user ? (
-					<>
-						<div>Nom: {user.pseudo}</div>
-						<div>Nom: {user.first_name}</div>
-						<div>
-							Age:{" "}
-							{user.birth_date?.toLocaleString() ?? "no data"}
-						</div>
-						<div>Localisation: {user.localisation}</div>
-						<div>Sexe: {user.gender}</div>
-						<div>Expérience: {user.experience}</div>
-					</>
-				) : (
-					<div>User not found :(</div>
-				)}
-				<div className="text-2xl font-bold ">
-					You are logged in as {session?.user?.email}
-				</div>
+  return (
+    <div className="flex h-screen">
+      <div className="w-screen h-screen flex flex-col space-y-5 justify-center items-center text-black">
+        <div>Search other profils from the database to try</div>
+        <Link href="/sanchezlori@example.com" className="text-textblue">
+          {" "}
+          for example sanchezlori@example.com
+        </Link>
+        <div>
+          Note that you have to be logged in to access this page and any profil
+          page
+        </div>
+        <h1 className="text-3xl font-bold ">Profil of {decodeEmail}</h1>
+        {user ? (
+          <div className="border-2 border-red-500 w-1/2">
+            <UpdateProfil user={user} />
+          </div>
+        ) : (
+          <div>User not found :</div>
+        )}
 
-				<Link
-					href={`/${session?.user?.email}`}
-					className="text-textblue"
-				>
-					Back to my profile
-				</Link>
-				<EditProfile />
-				<SignOut />
-			</div>
-		</div>
-	);
+        <EditProfile />
+        <SignOut />
+      </div>
+    </div>
+  );
 }
--- app/[profil]/updateprofil.tsx
+++ app/[profil]/updateprofil.tsx
@@ -1,168 +1,31 @@
-import { Button } from '@/components/ui/button';
-import clientPromise from '@/lib/mongodb';
-import React, { useState } from 'react';
-import {Input} from '@/components/ui/button';
-// import { useDispatch, useSelector } from "react-redux";
+"use client"
+import { User } from "@/types/tables";
+import { cn } from "@/lib/utils";
+import { Input } from "@/components/ui/button";
+import { useState } from "react";
 
-export async function getUser(email: string) {
-	const client = await clientPromise;
-	const db = client.db("geets");
-	const user = await db.collection("users").findOne({ email });
-	return user;
+interface UpdateProfilProps {
+  className?: string;
+  user: User;
 }
-// export function UpdateProfile({ user }) {
-//     // Utilisez les états locaux pour les valeurs des champs du formulaire
-//     const [formValues, setFormValues] = useState({
-//       name: user.name,
-//       last_name: user.last_name,
-//       age: user.age,
-//       location: user.location,
-//       sexe: user.sexe,
-//       experience: user.experience,
-//     });
-  
-//     // Fonction de gestion du changement des champs
-//     const handleChange = (e) => {
-//       const { name, value } = e.target;
-//       setFormValues({ ...formValues, [name]: value });
-//     };
-  
-//     // Fonction pour soumettre les modifications
-//     const handleSubmit = async (e) => {
-//       e.preventDefault();
-//       // Logique pour mettre à jour le profil dans la base de données
-//       const client = await clientPromise;
-//       const db = client.db("geets");
-//       await db.collection("users").updateOne(
-//         { email: user.email },
-//         { $set: formValues }
-//       );
-//     };
-  
-//     return (
-//       <form onSubmit={handleSubmit}>
-//         <Label>Nom:</Label>
-//         <Input
-//           type="text"
-//           name="name"
-//           value={formValues.name}
-//           onChange={handleChange}
-//         />
-//         <Label>Prénom:</Label>
-//         <Input
-//           type="text"
-//           name="last_name"
-//           value={formValues.last_name}
-//           onChange={handleChange}
-//         />
-//         <Label>Age:</Label>
-//         <Input
-//           type="number"
-//           name="age"
-//           value={formValues.age}
-//           onChange={handleChange}
-//         />
-//         <Label>Localisation:</Label>
-//         <Input
-//           type="text"
-//           name="location"
-//           value={formValues.location}
-//           onChange={handleChange}
-//         />
-//         <Label>Sexe:</Label>
-//         <Input
-//           type="text"
-//           name="sexe"
-//           value={formValues.sexe}
-//           onChange={handleChange}
-//         />
-//         <Label>Expérience:</Label>
-//         <Input
-//           type="text"
-//           name="experience"
-//           value={formValues.experience}
-//           onChange={handleChange}
-//         />
-//         <Button type="submit">Mettre à jour</Button>
-//       </form>
-//     );
-//   }
 
-export default function UpdateProfil({ user }) {
-	// Utilisez les états locaux pour les valeurs des champs du formulaire
-	const [formValues, setFormValues] = useState({
-	  name: user.name,
-	  last_name: user.last_name,
-	  age: user.age,
-	  location: user.location,
-	  sexe: user.sexe,
-	  experience: user.experience,
-	});
-  
-	// Fonction de gestion du changement des champs
-	const handleChange = (e: { target: { name: any; value: any; }; }) => {
-	  const { name, value } = e.target;
-	  setFormValues({ ...formValues, [name]: value });
-	};
-  
-	// Fonction pour soumettre les modifications
-	const handleSubmit = async (e: { preventDefault: () => void; }) => {
-	  e.preventDefault();
-	  // Logique pour mettre à jour le profil dans la base de données
-	  const client = await clientPromise;
-	  const db = client.db("geets");
-	  await db.collection("users").updateOne(
-		{ email: user.email },
-		{ $set: formValues }
-	  );
-	  // Vous pouvez ajouter une gestion des erreurs et des messages de succès ici
-	};
-  
-	return (
-	  <form onSubmit={handleSubmit}>
-		<div>Nom:</div>
-		<Input
-		  type="text"
-		  name="name"
-		  value={formValues.name}
-		  onChange={handleChange}
-		/>
-		<div>Prénom:</div>
-		<Input
-		  type="text"
-		  name="last_name"
-		  value={formValues.last_name}
-		  onChange={handleChange}
-		/>
-		<div>Age:</div>
-		<Input
-		  type="number"
-		  name="age"
-		  value={formValues.age}
-		  onChange={handleChange}
-		/>
-		<div>Localisation:</div>
-		<Input
-		  type="text"
-		  name="location"
-		  value={formValues.location}
-		  onChange={handleChange}
-		/>
-		<div>Sexe:</div>
-		<Input
-		  type="text"
-		  name="sexe"
-		  value={formValues.sexe}
-		  onChange={handleChange}
-		/>
-		<div>Expérience:</div>
-		<Input
-		  type="text"
-		  name="experience"
-		  value={formValues.experience}
-		  onChange={handleChange}
-		/>
-		<Button type="submit">Mettre à jour</Button>
-	  </form>
-	);
-  }
+export default function UpdateProfil({ className, user }: UpdateProfilProps) {
+  const [onEdit, setOnEdit] = useState<boolean>(false);
+  const [userEdited, setUserEdited] = useState<User>(user);
+
+  return (
+    <div className={cn("flex justify-center items-center", className)}>
+      <div className="flex flex-col space-y-5">
+        <h1 className="text-3xl font-bold">My Profile</h1>
+        <Input 
+
+          type="text"
+          name="email"
+          value={userEdited.email}
+          onChange={(e) => setUserEdited({ ...userEdited, email: e.target.value })}
+        />
+
+      </div>
+    </div>
+  );
+}
