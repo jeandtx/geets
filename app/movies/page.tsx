de6668feff1a2f9d1b8835b54b540d3735f56a3f
nicvlt <nicolas75d@gmail.com>
2024-05-16 10:44:52 +0200

    Fix : random (avoid conflicts)

--- app/movies/page.tsx
+++ app/movies/page.tsx
@@ -1,30 +1,27 @@
-import { Metadata } from "next";
-import clientPromise from "../../lib/mongodb";
-import SideProjectCard from "@/components/project_card";
-import { getPosts } from "@/lib/actions";
+import { Metadata } from 'next'
+import clientPromise from '../../lib/mongodb'
+import { getPosts } from '@/lib/actions'
 
 export const metadata: Metadata = {
-	title: "Top 15 Movies by Metacritic",
-};
+    title: 'Top 15 Movies by Metacritic',
+}
 
 export default async function Dashboard() {
-	const posts = await getPosts();
-	const postList = posts.map((post) => (
-		<>
-			<div className="flex p-8 bg-gray-50  overflow-hidden">
-				<SideProjectCard post={post} />
-			</div>
-		</>
-	));
+    const posts = await getPosts()
+    const postList = posts.map((post) => (
+        <>
+            <div className='flex p-8 bg-gray-50  overflow-hidden'></div>
+        </>
+    ))
 
-	return (
-		<div className="py-8">
-			<h1 className="title text-green-400 text-center text-3xl font-bold mb-16">
-				Top 15 posts by Metacritic
-			</h1>
-			<ul className="flex flex-wrap justify-center m-0 p-0 list-none">
-				{postList}
-			</ul>
-		</div>
-	);
+    return (
+        <div className='py-8'>
+            <h1 className='title text-green-400 text-center text-3xl font-bold mb-16'>
+                Top 15 posts by Metacritic
+            </h1>
+            <ul className='flex flex-wrap justify-center m-0 p-0 list-none'>
+                {postList}
+            </ul>
+        </div>
+    )
 }
