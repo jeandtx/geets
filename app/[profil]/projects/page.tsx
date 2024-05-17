d4be274097fab50fa31013071790f137960cb51f
soanja <soanja.mirindra.johanna.ravalisaona@efrei.net>
2024-05-17 16:55:47 +0200

    implemented the ProjectsPage that display all the projects of an user

--- app/[profil]/projects/page.tsx
+++ app/[profil]/projects/page.tsx
@@ -1,44 +1,32 @@
-import clientPromise from "@/lib/mongodb";
 import { Metadata } from "next";
 import { auth } from "@/app/auth";
+import { getProjects } from "@/lib/actions";
+import Link from "next/link";
 
 export const metadata: Metadata = {
 	title: "All Projects",
 };
 
-async function getUser(email: string) {
-	const client = await clientPromise;
-	const db = client.db("geets");
-	const user = await db.collection("users").findOne({ email });
-	return user;
-}
-async function getProjects() {
-	try {
-		const client = await clientPromise;
-		const db = client.db("geets");
-		const projects = await db.collection("projects").find({}).toArray();
-		return projects;
-	} catch (err) {
-		console.error("Error fetching projects:", err);
-		return [];
-	}
-}
-
 export default async function ProjectsPage() {
-	const projects = await getProjects();
-
-	("use client");
+	const session = await auth();
+    const userEmail = session?.user?.email;
+	if (!userEmail) {
+        return <div>Please log in to view your projects.</div>;
+    }
+    const projects = await getProjects(userEmail);
 
 	const projectList = projects
-		// .filter((project) => project.title && project.description)
+		.filter((project) => project.title && project.description && project.owner !== null)
 		.map((project) => (
 			<div
 				key={project.title}
 				className="w-[300px] bg-black-400 rounded-lg shadow-md m-2 p-2 text-center"
 			>
 				<div>
-					<div className="text-green-500 text-xl font-extrabold">
-						{project.title}
+					<div className="text-xl font-bold">
+						<Link href={`/${encodeURIComponent(userEmail)}/${encodeURIComponent(project.title)}`}>
+								{project.title}
+						</Link>
 					</div>
 					<div>Owner: {project.owner}</div>
 					<div>Description: {project.description}</div>
@@ -55,7 +43,7 @@ export default async function ProjectsPage() {
 
 	return (
 		<div className="py-8">
-			<div className="title text-green-400 text-center text-3xl font-bold mb-16">
+			<div className="title text-center text-3xl font-bold mb-16">
 				All projects
 			</div>
 			<div className="flex flex-wrap justify-center m-0 p-0 list-none">
