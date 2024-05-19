9b28bed829ec6ecc9d2ce452c79b9c8fd5fa19d2
nicvlt <nicolas75d@gmail.com>
2024-05-20 00:50:16 +0200

    Refactor: added padding

--- components/feed.tsx
+++ components/feed.tsx
@@ -5,7 +5,7 @@ import React from 'react'
 
 export default function Feed() {
     return (
-        <div className='flex flex-col w-75 h-full  bg-gray-200 space-y-8'>
+        <div className='flex flex-col max-w-75 h-full space-y-8 py-4 px-4'>
             <InputPost />
             <InfiniteScroll fetchFunction={getPosts} />
         </div>
