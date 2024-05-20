5e64dd0c49f27dceaf3777356a2bd879f0414cef
jeandtx <jean.doutriaux@gmail.com>
2024-05-20 17:13:43 +0200

    delete: logs

--- app/[profil]/[project]/page.tsx
+++ app/[profil]/[project]/page.tsx
@@ -10,18 +10,13 @@ export default async function ProjectPage({
 	const { profil } = params;
 	const decodeEmail = decodeURIComponent(profil);
 
-	console.log("Decoded email:", decodeEmail);
 	const user = await getUser(decodeEmail);
-	console.log("Fetched user:", user);
 
 	const projectId = params.project;
-	console.log("Project ID:", projectId);
 
 	const project: Project | null = await getProject(projectId);
-	console.log("Fetched project:", project);
 
 	const posts: Post[] = await getPostsByProjectId(projectId);
-	console.log("Fetched posts:", posts);
 
 	return (
 		<div className="">
