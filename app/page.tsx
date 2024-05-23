71da3ef01cd729cba3e718dcce87b3b387e8f1e9
cleophass <cleophas.fournier@efrei.net>
2024-05-23 20:10:58 +0200

    removing useless thing

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,11 @@
 
 import { Sidebar } from '@/components/sidebar'
-import InfiniteScroll from '@/components/infinitescroll'
 import { getPosts } from '@/lib/actions'
 
 export default async function Home() {
 	return (
-		<div className="flex w-full h-full bg-gray-200 ">
-			<InfiniteScroll fetchFunction={getPosts} />
-			<div className="h-full bg-red-500 w-1/5">aaa</div>
+		<div className="flex w-full h-full">
+			<div>hello Maya</div>
 		</div>
 	);
 }
