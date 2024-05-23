033a130dd31b30a09fd3f49388d18b79efe8b39d
cleophass <cleophas.fournier@efrei.net>
2024-05-23 20:22:56 +0200

    first try on mobile

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,10 @@
-
-import { Sidebar } from '@/components/sidebar'
-import { getPosts } from '@/lib/actions'
+import { Sidebar } from '@/components/sidebar';
+import { getPosts } from '@/lib/actions';
 
 export default async function Home() {
-	return (
-		<div className="flex w-full h-full">
-			<div>hello Maya</div>
-		</div>
-	);
+    return (
+        <div className="flex items-center justify-center w-full h-screen">
+            <div>hello Maya</div>
+        </div>
+    );
 }
