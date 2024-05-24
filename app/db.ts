4ff5e3e0132128566c73b254fb3f04b45129993a
cleophass <cleophas.fournier@efrei.net>
2024-05-24 10:12:44 +0200

    security add to wait the connection

--- app/db.ts
+++ app/db.ts
@@ -1,33 +1,41 @@
-import { MongoClient, MongoClientOptions, Db, Collection } from 'mongodb';
+import { MongoClient, Db, Collection } from 'mongodb';
 import { genSaltSync, hashSync } from 'bcrypt-ts';
 import clientPromise from '@/lib/mongodb';
 
-const MONGODB_URI = process.env.MONGODB_URI!;
-
-let db: Db, users: Collection;
+let db: Db;
+let users: Collection;
 
 async function connectToDatabase() {
     try {
         const client = await clientPromise;
-        db = client.db("bodyscan"); // database name
+        db = client.db("bodyscan");
         users = db.collection('users');
         console.log("Connected to MongoDB");
     } catch (error) {
         console.error("Error connecting to MongoDB:", error);
+        // Vous pouvez ajouter ici une logique de gestion des erreurs
     }
 }
 
+// Appeler la fonction connectToDatabase pour établir la connexion au démarrage de l'application
 connectToDatabase();
 
 export async function getUser(email: string) {
-    console.log("Attempting to find user herewith email:", email);
+    // Attendre que la connexion à la base de données soit établie avant d'exécuter l'opération
+    await connectToDatabase();
+
+    console.log("Attempting to find user with email:", email);
     return await users.findOne({ email });
 }
 
 export async function createUser(email: string, password: string) {
     try {
+        // Attendre que la connexion à la base de données soit établie avant d'exécuter l'opération
+        await connectToDatabase();
+
         let salt = genSaltSync(10);
         let hash = hashSync(password, salt);
+
         // Initialiser les champs avec des valeurs par défaut
         const newUser = {
             email,
