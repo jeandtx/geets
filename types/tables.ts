004f93df3ae152eb2b6bebe3afb017bdccac3169
Shadow032020 <asma200318@gmail.com>
2024-06-02 21:30:46 +0200

    UI for edit profile

--- types/tables.ts
+++ types/tables.ts
@@ -29,18 +29,17 @@ export interface User {
     password: string;
     pseudo?: string;
     projectIds?: string[];
-    labels?: string[];
+    labels?: string[]; //copy dtx code (texte entry) ??
     profil_picture?: string;
-    first_name?: string;
+    name?: string;
     last_name?: string;
-    birth_date: Date;
-    localisation?: string;
-    gender?: string;
-    experience?: string;
-    available?: string;
-    mobile?: string;
-    allowEmails?: boolean;
-    created?: Date;
+    birth_date?: Date; //date picker !!!!!!!!!!!!!!!!!!
+    location?: string;
+    sexe?: string; //selectbox f or m or jsp
+    experience?: number; //slider (0 to 10) !!!!!!!!!!!!!!!!!!!!
+    chercheARejoindreUnProjet?: boolean; //yes or no !!!!!!!!!!!!!!!!
+    mobile?: string; //+33 ...(enter only 10 numbers)
+    createdAt?: Date;
 }
 
 // export interface Participate{
