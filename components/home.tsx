73f5295dfc1029977bfbb56be56cfe349503cf59
soanja <soanja.mirindra.johanna.ravalisaona@efrei.net>
2024-06-07 04:00:24 +0200

    Added tabs in home page

--- components/home.tsx
+++ components/home.tsx
@@ -3,6 +3,7 @@ import { getPosts } from "@/lib/data/post";
 import { InputPost } from "@/components/inputpost";
 import React from "react";
 import SearchComponent from "./searchBar";
+import { UpperBar } from "./upper-bar";
 export default function Home() {
 	return (
 		<div className="flex h-screen bg-gray-100 sm:w-full">
@@ -11,8 +12,7 @@ export default function Home() {
 				<SearchComponent/>
 				</div>
 				<div className="flex flex-col space-y-4 ">
-				<InputPost />
-				<InfiniteScroll fetchFunction={getPosts} />$
+				<UpperBar />
 				</div>
 			</div>
 			<div className="sm:block w-0 sm:w-1/4 sm:min-w-48 space-y-5 sm:pl-5 flex flex-col ">
