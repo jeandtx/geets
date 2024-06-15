0c6e8654e92e8a7b74f43e51ca41edd2b73dadcc
cleophass <cleophas.fournier@efrei.net>
2024-06-15 22:10:23 +0200

    fix for mobile version

--- app/layout.tsx
+++ app/layout.tsx
@@ -39,6 +39,7 @@ export default function RootLayout({
         <link rel="apple-touch-icon" sizes="180x180" href="@/public/icons/icon-980x980.png" />
         <link rel="apple-touch-icon" sizes="192x192" href="/icons/icon-980x980.png" />
         <link rel="apple-touch-icon" sizes="512x512" href="/icons/icon-980x980.png" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
       </head>
       <body className={inter.className}>
         <div className="flex w-full h-screen overflow-hidden">
