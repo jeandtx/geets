fde0261a86ee398dc6b4d03f4edc441b33810118
soanja <soanja.mirindra.johanna.ravalisaona@efrei.net>
2024-06-20 09:18:06 +0200

    Update project page to include settings button to redirect user to settings page for its project

--- app/[profil]/[project]/page.tsx
+++ app/[profil]/[project]/page.tsx
@@ -1,27 +1,55 @@
-// page.tsx
+"use client";
+import { useState, useEffect } from 'react';
 import { getProject } from "@/lib/data/project";
 import { getPosts } from "@/lib/data/post";
 import { Post, Project } from "@/types/tables";
 import ProjectDetails from "@/components/project-details";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { useSession } from "next-auth/react";
 
-export default async function ProjectPage({
-	params,
+export default function ProjectPage({
+    params,
 }: Readonly<{
-	params: { profil: string; project: string };
+    params: { profil: string; project: string };
 }>) {
-	const projectId = params.project;
-	const project: Project | null = await getProject(projectId);
-	const posts: Post[] = await getPosts(-1, {
-		"project._id": projectId,
-	});
+    const projectId = params.project;
+    const profileId = params.profil;
+    const [project, setProject] = useState<Project | null>(null);
+    const [posts] = useState<Post[]>([]);
+    const [isLoading, setIsLoading] = useState(true);
+    const { data: session } = useSession();
 
-	return (
-		<div className="">
-			{project ? (
-				<ProjectDetails project={project} posts={posts} />
-			) : (
-				<div>No Project found 😔</div>
-			)}
-		</div>
-	);
+    useEffect(() => {
+        const fetchData = async () => {
+            setIsLoading(true);
+            const fetchedProject = await getProject(projectId);
+            setProject(fetchedProject);
+            setIsLoading(false);
+        };
+        fetchData();
+    }, [projectId]);
+
+    if (isLoading) {
+        return <div>Loading...</div>;
+    }
+
+    return (
+        <div className="">
+            {project ? (
+                <>
+                	<ProjectDetails project={project} posts={posts} />
+                    {session?.user?.email === decodeURIComponent(profileId) && (
+                        <Link href={`/${profileId}/${projectId}/settings`}>
+                            <Button variant="link">
+                                Settings
+                            </Button>
+                        </Link>
+                    )}
+                </>
+            ) : (
+                <div>No Project found 😔</div>
+            )}
+        </div>
+    );
 }
