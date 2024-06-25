f4bcf14903a0a8d9d20ca417005c3b596c711bca
nicvlt <nicolas75d@gmail.com>
2024-06-25 16:13:18 +0200

    Fix: harmonize Sign up & Sign in

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -48,9 +48,9 @@ export default function Login() {
     }
 
     return (
-        <div className='flex h-full w-full items-center justify-center'>
-            <div className='z-10 w-full max-w-md overflow-hidden rounded-2xl border border-gray-100 shadow-xl bg-white'>
-                <div className='flex flex-col items-center justify-center space-y-3 px-4 py-6 pt-8 text-center sm:px-16'>
+        <div className='flex w-full items-center justify-center'>
+            <div className='z-10 w-full max-w-md overflow-hidden rounded-xl border border-slate-200 bg-white'>
+                <div className='flex flex-col items-center justify-center space-y-3 px-4 py-6 pt-8 text-center sm:px-16	'>
                     <h3 className='text-xl font-semibold'>S&apos;inscire</h3>
                     <div className='text-sm text-gray-500'>
                         Rejoignez notre plateforme et découvrez de nouveaux
